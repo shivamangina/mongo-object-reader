9c1406196fa59849afd3d31af6f5411ca6634628
Shivakumar Mangina <shiva2nani.mangina@gmail.com>
2020-04-12 12:50:42 +0530

    create object create

---
+++ index.js
@@ -0,0 +1,18 @@
+const createObjectID = () => {
+  return function () {
+    let timestamp = ((new Date().getTime() / 1000) | 0).toString(16);
+    return (
+      timestamp +
+      "xxxxxxxxxxxxxxxx"
+        .replace(/[x]/g, function () {
+          return ((Math.random() * 16) | 0).toString(16);
+        })
+        .toLowerCase()
+    );
+  };
+};
+
+const readObjectID = (ObjectID) => {
+  return ObjectID;
+};
+module.export = { createObjectID, readObjectID };
